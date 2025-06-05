b32bcce188d379b6fa9df47277217bad538b76b3
331282 <148924183+331282@users.noreply.github.com>
2025-06-04 19:29:35 -0500

    Add sim
    sim works

--- src/main/java/frc/robot/subsystems/coral/CoralIOSim.java
+++ src/main/java/frc/robot/subsystems/coral/CoralIOSim.java
@@ -4,5 +4,32 @@
 
 package frc.robot.subsystems.coral;
 
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.mechanism.LoggedMechanism2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismLigament2d;
+import org.littletonrobotics.junction.mechanism.LoggedMechanismRoot2d;
+
 /** Add your docs here. */
-public class CoralIOSim implements CoralIO {}
+public class CoralIOSim implements CoralIO {
+
+  private LoggedMechanism2d armCanvas;
+  private LoggedMechanismRoot2d armJoint;
+  private LoggedMechanismLigament2d arm;
+
+  public CoralIOSim() {
+    armCanvas = new LoggedMechanism2d(6, 18);
+    armJoint = armCanvas.getRoot("Arm", 1, 1);
+    arm = armJoint.append(new LoggedMechanismLigament2d("Arm", 1, 90));
+  }
+
+  @Override
+  public void updateInputs(CoralIOInputs inputs) {
+    inputs.armAngle = arm.getAngle();
+    Logger.recordOutput("Arm", armCanvas);
+  }
+
+  @Override
+  public void moveArm(double angle) {
+    arm.setAngle(angle * 360 / (2 * Math.PI));
+  }
+}
